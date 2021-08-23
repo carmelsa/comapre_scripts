31f922d22bbbb307ed677828b272a56292bd39e2
carmelsahar <carmelsahar1994@gmail.com>
2021-08-23 14:25:09 +0300

    fixing print + ignore + partner permissions

--- compare_db_create_table_to_file.py
+++ compare_db_create_table_to_file.py
@@ -15,6 +15,9 @@ def connect_db(params):
 
 ignore_spaces = False
 ignore_capital = False
+ignore_auto_increment = False
+ignore_default_null = False
+ignore_int_and_tiny_int = False
 
 
 def get_table_names_from_db(my_db):
@@ -50,47 +53,52 @@ def get_table_content_by_name_from_db(my_db, table_name):
 def compare_content(table_content_db, table_content_file, table_name):
     table_content_db_lines = [line.strip() for line in table_content_db.split("\n")]
     table_content_file_lines = [line.strip() for line in table_content_file.split("\n")]
-    start = "**************************************\nfound difference in create table " + table_name + ":\n"
+    start = "found difference in create table " + table_name + ":\n"
     find_diff(table_content_db_lines, table_content_file_lines, start)
 
 
-def is_diff_spaces(set_db, set_file):
-    if ignore_spaces and len(set_db) != 0 and len(set_file) != 0:
-        text1 = [x.replace(' ', '') for x in set_db]
-        text2 = [x.replace(' ', '') for x in set_file]
-        return len(set(text1) ^ set(text2)) == 0
-    return False
+def check_ignores(line):
+    if ignore_capital:
+        line = line.lower()
+    if ignore_auto_increment:
+        line = re.sub(r'auto_increment(=\d*)?', '', line)
+    if ignore_default_null:
+        line = line.replace("default null", '')
+    if ignore_spaces:
+        line = line.replace(' ', '')
+    if ignore_int_and_tiny_int:
+        line = re.sub(r'int\(\d*\)', '', line)
+        line = re.sub(r'tinyint\(\d*\)', '', line)
+
+    return line
 
 
-def is_diff_capital(set_db, set_file):
-    if ignore_capital and len(set_db) != 0 and len(set_file) != 0:
-        text1 = [x.lower() for x in set_db]
-        text2 = [x.lower() for x in set_file]
-        return len(set(text1) ^ set(text2)) == 0
-    return False
+def check_if_diff_ignored(set_db, set_file):
+    if len(set_db) == 0 or len(set_file) == 0:
+        return False
+    test_db = [check_ignores(x) for x in set_db]
+    test_file = [check_ignores(x) for x in set_file]
+    return len(set(test_db) ^ set(test_file)) == 0
 
 
 def find_diff(set_db, set_file, start):
     diff_from_db = set(set_db) - set(set_file)
     diff_from_file = set(set_file) - set(set_db)
-    if is_diff_spaces(set_db, set_file) or is_diff_capital(set_db, set_file):
+    if check_if_diff_ignored(set_db, set_file):
         return
     if diff_from_db or diff_from_file:
-        print(start)
+        print("\n****************************************************************************\n")
+        print('\033[96m' + start + '\033[0m')
     if diff_from_db:
-        print("In db   - " + str(diff_from_db))
+        print('\033[94m' + "In db   - " + '\033[0m' + str(diff_from_db))
     if diff_from_file:
-        print("In file - " + str(diff_from_file))
+        print('\033[94m' + "In file - " + '\033[0m' + str(diff_from_file))
 
 
 def main():
-    global ignore_spaces, ignore_capital
-    f = open("compare_db_table_to_file.json", "r")
-    params = json.load(f)
-    ignore_spaces = params.get("ignore_spaces", False)
-    ignore_capital = params.get("ignore_capital", False)
-
-    with open(params["create_table_file_path"], "r") as fd:
+    params = load_param()
+    base_path = params.get("base_path")
+    with open(base_path + params["create_table_file_path"], "r") as fd:
         content = fd.read()
     file_tables = get_table_names_from_file(content)
     my_db = connect_db(params)
@@ -101,6 +109,18 @@ def main():
         table_content_db = get_table_content_by_name_from_db(my_db, table_name)
         compare_content(table_content_db[0], table_content_by_name.get(table_name), table_name)
     print("done")
+
+
+def load_param():
+    global ignore_spaces, ignore_capital, ignore_auto_increment, ignore_default_null, ignore_int_and_tiny_int
+    f = open("compare_db_table_to_file.json", "r")
+    params = json.load(f)
+    ignore_spaces = params.get("ignore_spaces", False)
+    ignore_capital = params.get("ignore_capital", False)
+    ignore_auto_increment = params.get("ignore_auto_increment", False)
+    ignore_default_null = params.get("ignore_default_null", False)
+    ignore_int_and_tiny_int = params.get("ignore_int_and_tiny_int", False)
+    return params
 
 
 if __name__ == '__main__':
--- compare_db_permissions_to_file.py
+++ compare_db_permissions_to_file.py
@@ -7,8 +7,11 @@ from copy import deepcopy
 
 import mysql.connector
 
+TYPE = "type"
+NAME = "name"
+PARTNER_ID = "partnerid"
 OBJECT_FIELD = "object"
-SERVER_FIELD = "service"
+SERVICE_FIELD = "service"
 SELECT_WITH_JOIN_PARAMETER = r"""
 select param_1 as object ,param_2 as parameter,param_3 as action,param_4,param_5 ,permission_item.partner_id as partnerid, 
 GROUP_CONCAT(DISTINCT name) as permissions
@@ -26,6 +29,13 @@ on permission_item.id=permission_to_permission_item.permission_item_id
 join permission on permission_to_permission_item.permission_id=permission.id 
 where param_1="%s" and permission_item.type="kApiActionPermissionItem"
 Group by param_1,param_2,param_4,param_3,param_5,permission_item.partner_id"""
+
+SELECT_WITH_JOIN_PARTNER = r"""select permission_item.partner_id as partnerid, permission.type ,
+GROUP_CONCAT(DISTINCT name) as permissions
+from permission_item join permission_to_permission_item 
+on permission_item.id=permission_to_permission_item.permission_item_id 
+join permission on permission_to_permission_item.permission_id=permission.id 
+where  permission_item.partner_id = %s Group by permission_item.partner_id, permission.type"""
 
 
 def connect_db(params):
@@ -47,88 +57,146 @@ def get_permission_from_db(my_db, object_name, SELECT_WITH_JOIN):
 def get_permission_names_from_file(db_permission, file_permissions_dict, file_permissions_delete, file_error_message):
     for key, file_permission in file_permissions_dict.items():
         if str_item(file_permission).lower() == str_item(db_permission).lower():
-            permissions_names = file_permission.get("permissions").split(",")
+            permissions_names = file_permission.get("permissions", file_permission.get("name")).split(",")
             del (file_permissions_delete[key])
             return {re.search(r"\s*(.*?>)?(?P<content>.*)\s*", x).group("content") for x in permissions_names}
 
-    file_error_message.append("missing configuration in file . db has permission - " + str_item(db_permission))
+    file_error_message.append(
+        '\033[94m' + "In db   - " + '\033[0m' + print_item(db_permission) + "file does not have it!")
 
 
-def find_diff_in_names(permission_names_from_db, permission_names_from_file, db_item, file_error_message):
+def find_diff_in_names(permission_names_from_db, permission_names_from_file, db_item):
     if not permission_names_from_db or not permission_names_from_file:
         return
     diff_from_db = permission_names_from_db - permission_names_from_file
     diff_from_file = permission_names_from_file - permission_names_from_db
+    return get_diff_error_message(db_item, diff_from_db, diff_from_file)
+
+
+def get_diff_error_message(db_item, diff_from_db, diff_from_file):
+    error_message = ""
     if diff_from_db or diff_from_file:
-        file_error_message.append(
-            "******************* found diff in permissions " + str_item(db_item) + " : *******************")
-    if diff_from_db:
-        file_error_message.append("In db   - " + str(diff_from_db))
-    if diff_from_file:
-        file_error_message.append("In file - " + str(diff_from_file))
+        error_message += "*********************************************\n" + '\033[96m' + "found diff in permissions " + '\033[0m' + "with " + print_item(
+            db_item) + ":\n"
+        error_message += '\033[94m' + "In db   - " + '\033[0m' + str(diff_from_db)
+        error_message += '\n\033[94m' + "In file - " + '\033[0m' + str(diff_from_file)
+        error_message += "\n*********************************************\n"
+    return error_message
 
 
 def str_item(file_item):
-    return file_item.get(OBJECT_FIELD) + " " + file_item.get("parameter", "") + " " + file_item.get(
-        "action") + " " + str(
-        file_item.get("partnerid"))
+    return file_item.get(OBJECT_FIELD, "") + " " + file_item.get("parameter", "") + " " + file_item.get(
+        "action", "") + " " + str(file_item.get(PARTNER_ID)) + " " + str(file_item.get("type", ""))
 
 
-def get_file_permissions_dict(section, config, file_error_message):
+def print_item(file_item):
+    string_to_print = ""
+    if file_item.get(
+            OBJECT_FIELD) is not None: string_to_print += '\033[97m' + "object\service: " + '\033[95m' + file_item.get(
+        OBJECT_FIELD) + ", "
+    if file_item.get(
+            "parameter") is not None: string_to_print += '\033[97m' + "parameter: " + '\033[95m' + file_item.get(
+        "parameter") + ", "
+    if file_item.get("action") is not None: string_to_print += '\033[97m' + "action: " + '\033[95m' + file_item.get(
+        "action") + ", "
+    if file_item.get(PARTNER_ID) is not None: string_to_print += '\033[97m' + PARTNER_ID + ": " + '\033[95m' + str(
+        file_item.get(PARTNER_ID)) + ", "
+    if file_item.get("type") is not None: string_to_print += '\033[97m' + "type: " + '\033[95m' + str(
+        file_item.get("type"))
+    return string_to_print + '\033[0m'
+
+
+def create_partner_dict(file_permission, partner_dict):
+    key = file_permission.get(PARTNER_ID) + "_" + file_permission.get(TYPE)
+    partner_dict[key][PARTNER_ID] = file_permission.get(PARTNER_ID)
+    partner_dict[key][TYPE] = file_permission.get(TYPE)
+    partner_dict[key][NAME] = file_permission.get(NAME) if partner_dict[key].get(NAME) is None \
+        else partner_dict[key][NAME] + "," + file_permission.get(NAME)
+
+
+def get_file_permissions_dict(section, config, key_field):
     file_permissions_dict = defaultdict(dict)
     for option in config.options(section):
-        split_option = option.replace("service", OBJECT_FIELD).split(".")
+        split_option = option.replace(SERVICE_FIELD, OBJECT_FIELD).split(".")
         file_permissions_dict[split_option[0]][split_option[1]] = config.get(section, option)
-    objects = {dict_item.get(OBJECT_FIELD, dict_item.get("service")) for dict_item in file_permissions_dict.values()}
-    if len(objects) != 1:
-        file_error_message.append("more than 1 object in file")
-    return file_permissions_dict, objects.pop()
+    objects = {dict_item.get(key_field, dict_item.get(SERVICE_FIELD)) for dict_item in file_permissions_dict.values()}
+    if key_field == PARTNER_ID:
+        partner_dict = defaultdict(dict)
+        {create_partner_dict(x, partner_dict) for x in file_permissions_dict.values()}
+        return partner_dict, set(objects)
+    return file_permissions_dict, set(objects)
+
+
+def check_section_type(section):
+    if section == "action_permission_items":
+        return SELECT_WITH_JOIN_ACTION, OBJECT_FIELD
+    elif section == "parameter_permission_items":
+        return SELECT_WITH_JOIN_PARAMETER, OBJECT_FIELD
+    elif section == "permissions":
+        return SELECT_WITH_JOIN_PARTNER, PARTNER_ID
+    return None, None
 
 
 def compare_db_to_file_permissions(my_db, path):
     file_error_message = []
+    diff_error_message = []
+    error_message = []
     config = configparser.ConfigParser()
     config.read(path)
     for section in config.sections():
-        if section == "action_permission_items":
-            compare_section_permissions(config, my_db, section, SELECT_WITH_JOIN_ACTION, file_error_message)
-        elif section == "parameter_permission_items":
-            compare_section_permissions(config, my_db, section, SELECT_WITH_JOIN_PARAMETER, file_error_message)
-        else:
-            file_error_message.append("unknown file type " + section)
+        query, key_field = check_section_type(section)
+        if query is None or key_field is None:
+            error_message.append("Unknown file type " + section)
             continue
-    if len(file_error_message) != 0:
-        print('\033[94m' + "\nfile : " + path + '\033[0m')
-        [print(x) for x in file_error_message]
+        file_permissions_dict, key_objects = get_file_permissions_dict(section, config, key_field)
+        for key_object in key_objects:
+            compare_section_permissions(my_db, query, file_error_message, diff_error_message, file_permissions_dict,
+                                        key_object)
+    print_error_messages(diff_error_message, file_error_message, error_message, path)
 
 
-def compare_section_permissions(config, my_db, section, select_query, file_error_message):
-    file_permissions_dict, object_name = get_file_permissions_dict(section, config, file_error_message)
+def print_error_messages(diff_error_message, file_error_message, error_message, path):
+    if len(file_error_message) != 0 or len(diff_error_message) != 0 or len(error_message) != 0:
+        print('\033[93m' + "\nfile : " + path + '\033[0m')
+        if len(error_message) != 0:
+            [print('\033[96m' + x) for x in set(filter(None, error_message))]
+            print("")
+        if len(file_error_message) != 0:
+            print('\033[96m' + "missing configuration:" + '\033[0m')
+            [print(x) for x in set(filter(None, file_error_message))]
+            print("")
+        if len(diff_error_message) != 0:
+            [print(x) for x in set(filter(None, diff_error_message))]
+
+
+def compare_section_permissions(my_db, select_query, file_error_message, diff_error_message, file_permissions_dict,
+                                object_name):
     db_permission_dict = get_permission_from_db(my_db, object_name, select_query)
     file_permissions_delete = deepcopy(file_permissions_dict)
-
     for db_item in db_permission_dict:
         permission_names_from_db = set(db_item.get("permissions").split(","))
         permission_names_from_file = get_permission_names_from_file(db_item, file_permissions_dict,
                                                                     file_permissions_delete, file_error_message)
-        find_diff_in_names(permission_names_from_db, permission_names_from_file, db_item, file_error_message)
+        diff_error_message.append(find_diff_in_names(permission_names_from_db, permission_names_from_file, db_item))
     if len(file_permissions_delete) != 0:
         for file_permission in file_permissions_delete.values():
-            file_error_message.append(
-                "missing configuration in db . file has permission - " + str_item(file_permission))
+            if file_permission.get(OBJECT_FIELD) == object_name:
+                file_error_message.append(
+                    '\033[94m' + "In file   - " + '\033[0m' + print_item(
+                        file_permission) + "db does not have it!")
 
 
 def main():
     with open("compare_db_table_to_file.json", "r") as f:
         params = json.load(f)
     my_db = connect_db(params)
-
+    base_path = params.get("base_path")
     folder_path = params.get("permission_folder_path")
     if folder_path:
-        for path in glob.glob(folder_path + "/*.ini"):
+        for path in glob.glob(base_path + folder_path + "/*.ini"):
             compare_db_to_file_permissions(my_db, path)
     elif params.get("permission_file_path"):
-        compare_db_to_file_permissions(my_db, params["permission_file_path"])
+        compare_db_to_file_permissions(my_db, base_path + params["permission_file_path"])
     else:
         print("please specify the file/folder path")
         exit(0)
