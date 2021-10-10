04a7a27cb8197d7cc917df10e655f439da1634c3
carmelsahar <carmelsahar1994@gmail.com>
2021-10-10 14:03:03 +0300

    jenkins file

--- generate_secrets_for_content.sh
+++ generate_secrets_for_content.sh
@@ -47,6 +47,9 @@ php server/tests/standAloneClient/exec.php server/deployment/base/scripts/init_c
 echo "start working on conversionProfile "
 php server/tests/standAloneClient/exec.php server/deployment/base/scripts/init_content/01.conversionProfile.99.xml
 
+echo "start working on entry.99 "
+php server/tests/standAloneClient/exec.php server/deployment/base/scripts/init_content/02.entry.99.xml
+
 #for file in `find .$INIT_CONTENT/ -not -name "*template*" -not -name "*UserRole*" `;do
 #        echo "start working on $file"
 #        php server/tests/standAloneClient/exec.php $file
