b7acfc86c1bd98a69cda702b7e59fce912d8dc4a
carmelsahar <carmelsahar1994@gmail.com>
2021-10-12 09:47:32 +0300

    jenkins file

--- generate_secrets_for_content.sh
+++ generate_secrets_for_content.sh
@@ -48,8 +48,12 @@ php server/tests/standAloneClient/exec.php server/deployment/base/scripts/init_c
 echo "start working on conversionProfile "
 php server/tests/standAloneClient/exec.php server/deployment/base/scripts/init_content/01.conversionProfile.99.xml
 
-echo "start working on entry.99 "
-php server/tests/standAloneClient/exec.php server/deployment/base/scripts/init_content/02.entry.99.xml
+#echo "start working on entry.99 "
+#php server/tests/standAloneClient/exec.php server/deployment/base/scripts/init_content/02.entry.99.xml
+
+
+echo "start working on entry. -4 "
+php server/tests/standAloneClient/exec.php server/deployment/base/scripts/init_content/02.entry.-4.xml
 
 #for file in `find .$INIT_CONTENT/ -not -name "*template*" -not -name "*UserRole*" `;do
 #        echo "start working on $file"
