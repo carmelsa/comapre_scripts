234ab5c8758414241639476e592e538303b7b73f
carmelsahar <carmelsahar1994@gmail.com>
2021-10-17 13:02:34 +0300

    jenkins file

--- generate_configuration_files.sh
+++ generate_configuration_files.sh
@@ -10,12 +10,12 @@ WEB_DIR="/opt/kaltura/web"
 
 SOURCE_FILE='/server/configuration/db.template.ini'
 DEST_FILE=`echo $SOURCE_FILE | sed 's@\(.*\)\.template\(.*\)@\1\2@'`
-sed -i "s#@DB1_HOST@#$DB_URL/#g" "s#@DB1_USER@#$DB_USER/#g" "s#@DB1_PASS@#$DB_PASSWORD/#g" "s#@DB1_PORT@#3306/#g" $DEST_FILE
+sed -e "s#@DB1_HOST@#$DB_URL/#g" -e "s#@DB1_USER@#$DB_USER/#g" -e "s#@DB1_PASS@#$DB_PASSWORD/#g" -e "s#@DB1_PORT@#3306/#g" $DEST_FILE
 
 SOURCE_FILE='/server/configuration/dc_config.template.ini'
 DEST_FILE=`echo $SOURCE_FILE | sed 's@\(.*\)\.template\(.*\)@\1\2@'`
-sed -i "s#@SERVICE_URL@#$SERVICE_URL/#g" "s#@WEB_DIR@#$WEB_DIR/#g" $DEST_FILE
+sed -e "s#@SERVICE_URL@#$SERVICE_URL/#g" -e "s#@WEB_DIR@#$WEB_DIR/#g" $DEST_FILE
 
 SOURCE_FILE='/server/configuration/local.template.ini'
 DEST_FILE=`echo $SOURCE_FILE | sed 's@\(.*\)\.template\(.*\)@\1\2@'`
-sed -i "s#@WWW_HOST@#$HOST/#g" "s#@WEB_DIR@#$WEB_DIR/#g" "s#@SERVICE_URL@#$SERVICE_URL/#g" "s#@DWH_HOST@#$DB_URL/#g" "s#@DWH_USER@#$DB_USER/#g" "s#@DWH_PASS@#$DB_PASSWORD/#g" "s#@DWH_PORT@#3306/#g" "s#@DWH_DATABASE_NAME@#kaltura/#g" $DEST_FILE
+sed -e "s#@WWW_HOST@#$HOST/#g" -e "s#@WEB_DIR@#$WEB_DIR/#g" -e "s#@SERVICE_URL@#$SERVICE_URL/#g" -e "s#@DWH_HOST@#$DB_URL/#g" -e "s#@DWH_USER@#$DB_USER/#g" -e "s#@DWH_PASS@#$DB_PASSWORD/#g" -e "s#@DWH_PORT@#3306/#g" -e "s#@DWH_DATABASE_NAME@#kaltura/#g" $DEST_FILE
